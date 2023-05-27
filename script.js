b8735a704c1190b3b79193103d0c41fe1c357f63
vasanthN2001 <vasanthnachimuthu@gmail.com>
2023-05-27 10:43:20 +0530

    updated

--- script.js
+++ script.js
@@ -3,24 +3,26 @@ const fetchUserDetail = () => {
     xhttp.onload = function () {
         const jsonObj = JSON.parse(this.responseText)
         console.log(jsonObj.length);
-    console.log(jsonObj);
+        console.log(jsonObj);
 
-    let html="";
-     
-    for(let i=0;i<jsonObj.length;i++)
-    {
-        const element = jsonObj[i];
-        
-console.log(i);
- html += '<div class="container"><p class="userName">' + element.account_number + '</p>' + '<span class="Name">' + element.bank_name
- + '</span>'
-   
-    }
-       
-document.getElementById("main").innerHTML=html;
+        let html = "";
+
+        for (let i = 0; i < jsonObj.length; i++) {
+            const element = jsonObj[i];
+
+            console.log(i);
+            html += '<div class="container"><p class="userName">' + element.account_number + '</p>' + '<span class="Name">' + element.bank_name
+                + '</span>' + "  "+ '<span class="Name">' + element.routing_number
+                + '</span>'
+
+
+        }
+
+        document.getElementById("main").innerHTML = html;
     };
-    xhttp.open("GET", "https://random-data-api.com/api/v2/banks?size=10", true);
+    xhttp.open("GET", "https://random-data-api.com/api/v2/banks?size=5", true);
     xhttp.send();
 
 };
 
+routing_number
