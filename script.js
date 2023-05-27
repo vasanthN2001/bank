03cc37642f3570a4910b8b3821d21e7c2e3e6dc9
vasanthN2001 <vasanthnachimuthu@gmail.com>
2023-05-27 10:17:04 +0530

    updated table

--- script.js
+++ script.js
@@ -12,27 +12,14 @@ const fetchUserDetail = () => {
         const element = jsonObj[i];
         
 console.log(i);
+ html += '<div class="container"><p class="userName">' + element.account_number + '</p>' + '<span class="Name">' + element.bank_name
+ + '</span>'
    
     }
        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById("main").innerHTML=html;
     };
-    xhttp.open("GET", "https://random-data-api.com/api/v2/users?size=10", true);
+    xhttp.open("GET", "https://random-data-api.com/api/v2/banks?size=10", true);
     xhttp.send();
 
 };
